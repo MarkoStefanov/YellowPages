9c59a8fad74ec98fd04cca0e599bb97f283f6966
Marko Stefanov <103323102+MarkoStefanov@users.noreply.github.com>
2025-03-26 22:39:38 +0000

    Add files via upload

---
+++ UCLYellowPages/profiles/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from . import views
+
+LOGOUT_REDIRECT_URL = 'login/'
+
+urlpatterns = [
+    path('edit_profile/', views.ProfileEditView.as_view(), name='profile-edit'),
+    path('search/', views.SearchView.as_view(), name='search'),
+    path('history/', views.HistoryView.as_view(), name='history'),
+    path('send-verification-code/', views.send_verification_code, name='send_verification_code'),
+
+]
