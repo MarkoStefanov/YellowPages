8c1fd4459be43924c199b609e65786c10a5c8f0d
jude-sph <49697990+jude-sph@users.noreply.github.com>
2025-03-28 13:24:43 +0000

    Delete UCLYellowPages directory

--- UCLYellowPages/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# Ensure the project root is in the Python path
-project_root = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, project_root)
-
-# Set the default settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UCLYellowPages.settings')
